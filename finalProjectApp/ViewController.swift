438e010f2be74df3cc63fd29e8892f9ebe7bd926
Lindsey Hirano <lindseysh11@gmail.com>
2020-07-14 23:24:30 -0700

    Initial Commit

---
+++ finalProjectApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  finalProjectApp
+//
+//  Created by lindsey hirano on 7/14/20.
+//  Copyright © 2020 Lindsey Hirano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
