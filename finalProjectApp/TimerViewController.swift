3069b8e9da4f43be7be43524ede001eae8777b24
Sophia Labrador <sophialabrador21@mayfieldsenior.org>
2020-07-16 12:06:50 -0700

    commit

---
+++ finalProjectApp/TimerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TimerViewController.swift
+//  finalProjectApp
+//
+//  Created by Sophia Labrador on 7/16/20.
+//  Copyright © 2020 Lindsey Hirano. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
