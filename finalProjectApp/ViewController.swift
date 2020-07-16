70184abdf5d9df497b0ae5bd1dce0e83cd57164a
Lindsey Hirano <lindseysh11@gmail.com>
2020-07-16 13:39:51 -0700

    Calendar

--- finalProjectApp/ViewController.swift
+++ finalProjectApp/ViewController.swift
@@ -6,15 +6,24 @@
 //  Copyright © 2020 Lindsey Hirano. All rights reserved.
 //
 
+import FSCalendar
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FSCalendarDelegate {
+    
+    @IBOutlet var calendar: FSCalendar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        calendar.delegate = self
     }
 
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE MM-dd-YYYY at h:mm a"
+        let string = formatter.string(from: date)
+        print("\(string)")
+    }
 
 }
 
