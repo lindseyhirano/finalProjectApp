80b0270705fbbfee29a50ed1bb93dc52114b3a6b
Sophia Labrador <sophialabrador21@mayfieldsenior.org>
2020-07-17 00:13:03 -0700

    commit

--- finalProjectApp/PlantPageViewController.swift
+++ finalProjectApp/PlantPageViewController.swift
@@ -10,13 +10,27 @@ import UIKit
 
 class PlantPageViewController: UIViewController {
 
+    
+    @IBOutlet weak var plantView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func grow1Tapped(_ sender: Any) {
+        plantView.image = UIImage(named: "stage2")
+    }
+    
+    @IBAction func grow2Tapped(_ sender: Any) {
+        plantView.image = UIImage(named: "stage3")
+    }
+    
+    @IBAction func grow3Tapped(_ sender: Any) {
+        plantView.image = UIImage(named: "stage4")
+    }
+    
     /*
     // MARK: - Navigation
 
