3ad611135fc273ad6d073dc676f9828b1311bf28
Jaya Athuluru <jathuluru@gmail.com>
2020-07-16 20:10:22 -0700

    timer

--- finalProjectApp/TimerViewController.swift
+++ finalProjectApp/TimerViewController.swift
@@ -9,7 +9,44 @@
 import UIKit
 
 class TimerViewController: UIViewController {
+    
+    var OurTimer = Timer()
+    var TimerDisplayed = 0
+    
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    
+    
+    @IBAction func StartBTN(_ sender: Any) {
+        OurTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(Action), userInfo: nil, repeats: true)
+    }
+    
+    
+    
+    @IBAction func PauseBTN(_ sender: Any) {
+        OurTimer.invalidate()
+    }
+    
+    
+    @IBAction func ResetBTN(_ sender: Any) {
+        OurTimer.invalidate()
+        TimerDisplayed = 0
+        Label.text = "0"
+    }
+    
+    @objc func Action() {
 
+              TimerDisplayed += 1
+              Label.text = String(TimerDisplayed)
+          }
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
